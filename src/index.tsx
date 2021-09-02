af1790fee159e7371b81fcc1daf8ad6facd1133f
jonahkiprop34@gmail.com <jonahkiprop34@gmail.com>
2021-09-02 13:50:26 +0300

    #bootsrap the app

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider, InMemoryCache, ApolloClient, NormalizedCacheObject, Reference} from '@apollo/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          People: {
+            keyArgs: false,
+            merge(existing , incoming) {
+              console.log(incoming);
+              let People: Reference[] = [];
+              if(existing && existing.People) {
+                // People = People.concat(existing.People)
+              }
+              if(incoming && incoming.People) {
+                People = People.concat(incoming.People);
+              }
+              return { ...incoming, People}
+            }
+          }
+        }
+      }
+    }
+  }),
+  uri: 'http://104.131.118.153:3500/',
+});
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client = {client}>
+      <Router>
+        <App/>
+      </Router>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
