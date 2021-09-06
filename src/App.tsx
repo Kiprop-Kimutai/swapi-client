c67a5f299b8e05d0df7e68fcdd55e33ea1cb6b20
jonahkiprop34@gmail.com <jonahkiprop34@gmail.com>
2021-09-06 12:35:10 +0300

    #remove App.css import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PersonDetails from './pages/Personal.Details';
